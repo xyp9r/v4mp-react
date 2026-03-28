ec9b4308b80cb856e5dbe45180bcd5a938832534
xyp9r <vanaradimov@gmail.com>
2026-03-28 11:32:31 +0100

    refactor: migrate ViewCounter to TypeScript

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import Clock from './components/Clock';
-import ViewCounter from './components/ViewCounter'
+import ViewCounter from './components/ViewCounter';
 import Matrix from './components/Matrix'
 import TypingText from './components/TypingText';
 import CustomCursor from './components/CustomCursor'
